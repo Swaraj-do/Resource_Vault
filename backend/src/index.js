3b60bc085c6d40cb894a3a60589dde766f19e2bf
Swaraj-do <swarajmohanty183@gmail.com>
2026-03-06 14:52:53 +0530

    fix: cors issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,9 @@ const limiter = rateLimit({
 
 app.use(limiter);
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: function(origin, callback) {
+    callback(null, true)
+  },
   credentials: true
 }));
 app.use(express.json());
